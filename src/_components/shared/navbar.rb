e362715a09dddac22748462cc80adc89bb8b3277
Kevin Pratt <kevin@paradem.co>
2022-12-07 21:16:39 -0700

    More quick changes

--- src/_components/shared/navbar.rb
+++ src/_components/shared/navbar.rb
@@ -1,5 +1,21 @@
 class Shared::Navbar < Bridgetown::Component
-  def initialize(metadata:, resource:)
-    @metadata, @resource = metadata, resource
+  def initialize(metadata:, resource:, inverse: false)
+    @metadata, @resource, @inverse = metadata, resource, inverse
+  end
+
+  def text_color
+    if @inverse
+      "text-black"
+    else
+      "text_white"
+    end
+  end
+
+  def button_color
+    if @inverse
+      "bg-gray-400 text-gray-800"
+    else
+      "bg-white text-gray-400"
+    end
   end
 end
