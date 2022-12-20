ffb541862eef724ce763e6bd5cbbf1b734370d8c
Kevin Pratt <kevin@paradem.co>
2022-12-19 22:17:21 -0700

    Data now flows.
    The tabs render and show up grouped properly.
    
    And the application knows about the different datasets!!
    
    Only problem left to figure out is the "closed" opportunities.  They
    will only refresh on a site rebuild....

--- plugins/builders/load_pages.rb
+++ plugins/builders/load_pages.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
+require "byebug"
+
 # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/BlockLength
 class Builders::LoadPages < SiteBuilder
+  def closed_opportunity?(opportunity)
+    debugger
+    return false if opportunity.closed_date.nil?
+
+    opportunity.closed_date < Time.now
+  end
+
   def build
     hook :site, :pre_render do
       index = site.data.dato.home
@@ -41,6 +50,26 @@ class Builders::LoadPages < SiteBuilder
           dato_object update
           layout "top_nav"
           content "*** I'm an update***"
+        end
+      end
+
+      site.data.dato.events.each do |event|
+        add_resource :pages, "/#{event.slug}.erb" do
+          title event.title
+          meta_type "event"
+          dato_object event
+          layout "top_nav"
+          content "*** I'm an event***"
+        end
+      end
+
+      site.data.dato.opportunities.each do |opportunity|
+        add_resource :pages, "/#{opportunity.slug}.erb" do
+          title opportunity.title
+          meta_type opportunity.close_date < Time.now.utc ? "closed_opportunity" : opportunity.opportunity_type
+          dato_object opportunity
+          layout "top_nav"
+          content "*** I'm an opportunity***"
         end
       end
     end
