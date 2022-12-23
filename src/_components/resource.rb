4164f199dbdd90394cd593e8d8e4bb77f48c1881
Kevin Pratt <kevin@paradem.co>
2022-12-22 23:57:47 -0700

    Some tweaks to resources

---
+++ src/_components/resource.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Resource < Bridgetown::Component
+  attr_reader :resource, :options
+
+  def initialize(resource:, options:)
+    @resource, @options = resource, options
+    super()
+  end
+
+  def resource_type(res)
+    case res.resource.first.resources.first.item_type.api_key
+    when "resource_link"
+      ExternalLink.new(resource: res, options:)
+    when "resource_file"
+      FileLink.new(resource: res, options:)
+    end
+  end
+end
